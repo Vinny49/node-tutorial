97d1f9594ce739b05376d2d201ed2b1ff18d53de
Vin DiDonato <vinny.didonato@gmail.com>
2012-12-01 23:39:32 -0500

    Updated files
    Added in functionality to upload images and added new request handler
    "show".

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-    var port = 8888;
-    
+    var port = 8888; // vd
     function onRequest( request,response ) {
-        var pathname = url.parse( request.url ).pathname; 			// use the url library to obtain the pathname (directory)
-        var qs = url.parse( request.url ).query;
+        var pathname = url.parse( request.url ).pathname; // use the url library to obtain the pathname (directory)
+        var qs = url.parse( request.url ).query; // vd
         console.log(Date() + " Request for " + pathname + " received.  Querystring is " + qs); 	// basic logging
-      response.writeHead(200, {"Content-Type": "text/html"});			// set content type header
-
-      route( handle, pathname, response );							// do something according to the pathname
+        route( handle, pathname, response, request );							// do something according to the pathname
     }
     
     http.createServer(onRequest).listen( port );				// listen on port
